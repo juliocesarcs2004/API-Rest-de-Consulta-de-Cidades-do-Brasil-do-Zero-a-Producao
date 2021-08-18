138c02b69ce58142859158fb80209a4cf822a2ca
Julio César <juliocesarcs2004@gmail.com>
2021-08-18 11:07:10 -0300

    Commit 9

---
+++ src/main/java/com/githubjuliocesarcs2004/citiesapi/staties/StateRepository.java
@@ -0,0 +1,6 @@
+package com.githubjuliocesarcs2004.citiesapi.staties;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
