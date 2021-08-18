bbfac0cf8d5dbd217f8fc81402cf884d5e77da4b
Julio César <juliocesarcs2004@gmail.com>
2021-08-18 13:04:11 -0300

    Commit 10

---
+++ src/main/java/com/githubjuliocesarcs2004/citiesapi/cities/CityRepository.java
@@ -0,0 +1,7 @@
+package com.githubjuliocesarcs2004.citiesapi.cities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+}
+
